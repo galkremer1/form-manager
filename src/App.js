77707620a4d7801fb594f2e841fcaedbfebc8ed0
galkremer1 <galkremer1@walla.co.il>
2018-10-26 22:27:31 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import FormExample from './components/form'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        <FormExample />
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
